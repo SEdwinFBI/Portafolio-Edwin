268697dddcee4286b70b519ed1b0c0191d7bb75a
Edwin Baquaix <baquiaxfernando0@gmail.com>
2024-03-25 14:05:22 -0600

    prueba

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import NavBar from "./components/NavBar";
+import Contactarme from "./components/Contactarme";
+import Inicio from "./components/Inicio";
+import Habilidades from "./components/Habilidades";
+import Proyectos from "./components/Proyectos";
+import SobreMi from "./components/SobreMi";
+import Style from './App.module.css';
+
+
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body className={Style}>
+      <NavBar  /> {/* Menus*/}
+      {/*//se uso container porque estamos usando bootrap, entonces lo que hace es indicar que es el contenedor de todo*/}
+      <div className=""> {/* cuerpo */}
+        <Inicio />
+        <SobreMi />
+        {/*<Habilidades />
+        <Proyectos />
+  <Contactarme />*/ }
+      </div> {/*fin Cuerpo*/}
+    </body>
+
   );
 }
 
---
+++ src/components/NavBar.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+
+const NavBar = () => {
+  return (
+
+    <nav className="navbar navbar-expand-md bg-dark sticky-top border-bottom " data-bs-theme="dark" >
+      <div className="container">
+        <a className="navbar-brand d-md-none" href="#hero">
+          <h1>Edwin Baquiax</h1>
+        </a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvas" aria-controls="offcanvas" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="offcanvas offcanvas-end" tabindex="-1" id="offcanvas" aria-labelledby="offcanvasLabel">
+          <div className="offcanvas-header">
+            <h5 className="offcanvas-title" id="offcanvasLabel">Menu</h5>
+            <button type="button"className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          </div>
+          <div class="offcanvas-body ">
+            <ul class="navbar-nav flex-grow-1 justify-content-between  text-center fs-5" data-bs-dismiss="offcanvas" >
+              <li class="nav-item"> <h2>Edwin Baquiax</h2>
+              </li>
+              <li className="nav-item"><a className="nav-link" href="#hero">inicio</a></li>
+              <li className="nav-item"><a className="nav-link" href="#sobremi">Sobre mi</a></li>
+              <li class="nav-item disabled"><a class="nav-link disabled" href="#">Muy pronto</a></li>
+              {/*<li className="nav-item"><a className="nav-link" href="#skills">Habilidades</a></li>
+              <li className="nav-item"><a className="nav-link" href="#works">Proyectos</a></li>
+  <li className="nav-item"><a className="nav-link" href="#contacto">Contactame</a></li>*/}
+     
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default NavBar
