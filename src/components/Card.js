a5ee294f96c7cfb2a8210e3cadf62bc3d916dad3
Edwin Baquaix <baquiaxfernando0@gmail.com>
2024-05-21 15:09:00 -0600

    Nuevo item

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,30 +1,45 @@
 import React from 'react'
 import estilos from '../App.module.css';
 interface Props {
-    // variables
-    imagenUrl: string;
-    titulo: string;
-    descripcion: string;
-    tech: string;
-    href: String;
+  // variables
+  imagenUrl: string;
+  titulo: string;
+  descripcion: string;
+  tech: string;
+  href: String;
 
+
+  
+
+
+}
+const Pc = () =>{
+  if (this.props.cel == "si") {
+    return (
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-phone" viewBox="0 0 16 16">
+    <path d="M11 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
+    <path d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2"/>
+  </svg>
+    )
   }
+ 
+}
 
 const Card = (Props) => {
 
   return (
     <div className='col mb-3 '>
-    <div className="card" id={estilos.carta}>
-      <img src={Props.imagenUrl} className="card-img-top" alt="..."/>
+      <div className="card" id={estilos.carta}>
+        <img src={Props.imagenUrl} className="card-img-top" alt="..." />
         <div className="card-body">
           <h5 className="card-title">{Props.titulo}</h5>
           <p className="card-text">{Props.descripcion}</p>
-          <h5 className="card-title">Tecnologias utilizadas</h5>
-          <p className="card-text">{Props.tech}</p>
+          <h5 className="card-title">Tecnologias utilizadas </h5> 
+          <p className="card-text">{Props.tech}</p> 
           <a href={Props.href} className="btn btn-primary" id={estilos.butt} target='blank'>Ir al proyecto</a>
         </div>
+      </div>
     </div>
-  </div>
   )
 }
 
--- src/components/Proyectos.js
+++ src/components/Proyectos.js
@@ -13,6 +13,13 @@ const Proyectos = () => {
         <h2 className='text-center'>proyectos basicos</h2>
         <div className='row row-cols-1 row-cols-sm-3 '> {/*inicio columnas y filas*/}
 
+
+          <Card imagenUrl='https://static.vecteezy.com/system/resources/previews/019/045/797/non_2x/owl-graphic-clipart-design-free-png.png'
+            titulo='Movie MDEE'
+            descripcion='Aplicacion para ver Peliculas, con el uso de la api de themoviebd '
+            href='https://sedwinfbi.github.io/pelis-prueba/'
+            tech='Vite, Boostrap, React js, api the moviebd' />
+
           <Card imagenUrl='https://summa.es/blog/wp-content/uploads/2018/09/iStock-865230556-1280x720.jpg'
             titulo='Diseño y Grabacion'
             descripcion='Pagina web de un estudio de diseño '
@@ -25,15 +32,17 @@ const Proyectos = () => {
             tech='HTML5, CSS3' />
           <Card imagenUrl='https://www.grupodigital.eu/blog/wp-content/uploads/2019/11/gamer-decorative-illustration-flat-design_23-2148250394-e1574340136197.jpg'
             titulo='Programa Ya!'
-            descripcion='Sitio web sobre servicios de programadores- -No Responsivo '
+            descripcion='Sitio web sobre servicios de programadores. -No Responsivo'
             href='https://sedwinfbi.github.io/programaya/'
-            tech='HTML5, CSS3' />
+            tech='HTML5, CSS3'
+          />
 
           <Card imagenUrl='https://i.pinimg.com/736x/20/d8/fb/20d8fb8426416fa8ed15749a7a821d16.jpg'
             titulo='Green tree'
             descripcion='Este fue una pagina que realize para el proyecto de seminario para mi graduacion como Perito Contador - creado a finales de 2022 '
             href='https://green-tree2022.netlify.app/'
-            tech='HTML5, CSS3' />
+            tech='HTML5, CSS3'
+          />
 
           <Card imagenUrl='https://m.media-amazon.com/images/I/51HrrEbfYWL._AC_SL1500_.jpg'
             titulo='Stick Lamp'
@@ -41,8 +50,8 @@ const Proyectos = () => {
             href='https://stik-lamp.netlify.app/'
             tech='HTML5, CSS3' />
 
-<Card imagenUrl='https://cdn5.dibujos.net/dibujos/pintados/201603/calculadora-solar-colegio-10377919.jpg'
-            titulo='Mi primier proyecto'
+          <Card imagenUrl='https://cdn5.dibujos.net/dibujos/pintados/201603/calculadora-solar-colegio-10377919.jpg'
+            titulo='Calculadora'
             descripcion='Este fue uno de los primer proyectos que realize, fue una calculadora basica pero funcional - creado a mediados de 2022 '
             href='https://edwinbxix.netlify.app/seccion/calculadora'
             tech='HTML5, CSS3, JAVASCRIPT' />
@@ -53,8 +62,8 @@ const Proyectos = () => {
         <div className='row row-cols-1 row-cols-sm-3 '>{/*inicio columnas y filas*/}
           <Card imagenUrl='https://cms.rootstack.com/sites/default/files/inline-images/Captura%20de%20pantalla%202023-08-02%20a%20la%28s%29%2013.20.50.png'
             titulo='Coming soon'
-            descripcion='En desarrollo, aplicaciones web o apis'
-            tech='se espera desarrollar con Java- Spring boot o React' />
+            descripcion='Proyecto de api rest con spring boot y MySQL,'
+            tech='MySQL, JAVA, Spring Boot, Docker' />
 
           <Card imagenUrl='https://cms.rootstack.com/sites/default/files/inline-images/Captura%20de%20pantalla%202023-08-02%20a%20la%28s%29%2013.20.50.png'
             titulo='Coming soon'
