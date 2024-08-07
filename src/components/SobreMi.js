f6a5336f6461a34f675031062872ca718768fa3a
Edwin Baquaix <baquiaxfernando0@gmail.com>
2024-08-06 23:14:10 -0600

    Sobre mi

--- src/components/SobreMi.js
+++ src/components/SobreMi.js
@@ -17,9 +17,10 @@ const SobreMi = () => {
       </div>
       <div className={Style.info_about}>
         <h3>Desarrollador de Software</h3>
-        <p className='container'> Soy un estudainte de Ing. en sistemas y ciencias de la computacion,
+        <p className='container'> Soy un estudiante de Ing. en sistemas y ciencias de la computacion,
           desde que tengo memoria siempre me ha gustado todo sobre informatica. Soy alguien autodidacta, 
-          eh venido aprendiendo y haciendo proyectos desde 2022, tengo la capacidad de aprender rapido y
+          eh venido aprendiendo y haciendo proyectos desde 2022, capacitando me en plataformas como Intecap
+          Udemy, Platzi y En el programa de Oracle Next Education, tengo la capacidad de aprender rapido y
           actualmente busco la primera oportunidad para desarrollarme en este area, no cuento con mucha
           experiencia pero si con una gran pasion por programacion.
         </p>
