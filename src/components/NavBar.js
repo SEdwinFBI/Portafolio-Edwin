086190dbb1364ac39ef0408b2e81952d7f7fecfa
EdwinBaquiax <104693627+SEdwinFBI@users.noreply.github.com>
2024-08-06 23:24:30 -0600

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -41,7 +41,7 @@ const NavBar = () => {
   return (
     <nav className="navbar navbar-expand-lg " id={style.navBar}>
       <div className="container-fluid"  >
-        <a className="navbar-brand" href="#hero">Navbar</a>
+        <a className="navbar-brand" href="#hero">Edwin B</a>
 
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" style={{ backgroundColor: " rgb(12, 147, 226)" }}>
           <svg xmlns="http://www.w3.org/2000/svg" width="35" height="25" fill="currentColor" class="bi bi-justify" viewBox="0 0 16 16">
