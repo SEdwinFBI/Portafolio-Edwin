b1fff0fb4de8f23e381fac279503127b45fd4dbc
Edwin Baquaix <baquiaxfernando0@gmail.com>
2024-11-27 22:13:36 -0600

    logo cooperativa coopenoj

--- src/utils/proyectos.ts
+++ src/utils/proyectos.ts
@@ -31,7 +31,7 @@ export const proyectos: proyectosProps[]=[
         proyectosDesarrollados: [
             {
                 id:23,
-                imagenUrl:"../assets/proy/logocoopenoj.jpg",
+                imagenUrl:"https://drive.google.com/file/d/1vDYVTzSJv9iU27oOfxs8jrhxw240Nkh6/view?usp=drive_link",
                 titulo:"Cooperativa Coopenoj",
                 descripcion:"En Desarrollo Sitio web Oficial de Cooperativa Coopenoj R.L.",
                 href:"https://coopenoj.netlify.app/",
